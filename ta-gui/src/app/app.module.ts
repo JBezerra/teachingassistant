92adcc79d6953ab059b0a1aea22dbbe87e305372
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:03:47 -0300

    fix(git): cherry pick erros fixed

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,11 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD:ta-gui/src/app/app.module.ts
-    AppRoutingModule
-=======
     FormsModule
->>>>>>> 8303e24... elementos do formulario para cadastro de alunos:gui/ta-gui/src/app/app.module.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
